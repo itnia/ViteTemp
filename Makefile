06dc64893665c66346819de16b14bdccfa0908ee
Sasha <itnia.aurey@outlook.com>
2024-08-09 01:39:55 +0300

    update

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@
 # TODO: make dev - запустить vite & php server
 # TODO: make break
 # TODO: make build
-# TODO: make deploy
-# TODO: make deploy_with_build - с процессом build
+# TODO: make deploy - с процессом build
 
 # php -S 127.0.0.1:4242 server/tools.php
--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,16 @@ function getHtmlEntries() {
     const entries = {};
 
     // Read all files in the directory
-    const files = fs.readdirSync(pagesDir);
+    // TODO: полные пути реализовать
+    const files = fs.readdirSync(pagesDir, { recursive: true });
 
     // Filter out HTML files
     const htmlFiles = files.filter((file) => file.endsWith(".html"));
 
     // Create entries for each HTML file
-    htmlFiles.forEach((file) => {
+    htmlFiles.forEach((file, index) => {
         const name = path.basename(file, ".html");
-        entries[name] = path.resolve(pagesDir, file);
+        entries[index] = path.resolve(pagesDir, file);
     });
 
     return entries;
