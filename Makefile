0fe15efa550a9eb4f14e55e824306c52a2da2e9c
Sasha <itnia.aurey@outlook.com>
2024-08-09 01:34:23 +0300

    update

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 # TODO: первое усановка зависемостей
 
 # TODO: make dev - запустить vite & php server
+# TODO: make break
 # TODO: make build
 # TODO: make deploy
 # TODO: make deploy_with_build - с процессом build
 
+# php -S 127.0.0.1:4242 server/tools.php
--- server/tools.php
+++ server/tools.php
@@ -1,3 +1,24 @@
 <?php
 
-# TODO: анализ файлов в /pages и вывод в поверхностной навигации - json api
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json; charset=utf-8');
+
+
+$rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(dirname(__DIR__) . '/pages'));
+$files = array();
+
+/** @var SplFileInfo $file */
+foreach ($rii as $file) {
+    if ($file->isDir()) {
+        continue;
+    }
+    if (str_ends_with($file->getPathname(), '.html')) {
+        $files[] = substr(str_replace('\\', '/', $file->getPathname()), strlen(dirname(__DIR__)));
+    }
+}
+
+$data = $files;
+
+echo json_encode($data);
